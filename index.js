eccf1466e957d3ef19ab87aff2a3920ec0af3937
jihad-hossain1 <jihadkham934@gmail.com>
2023-04-29 22:01:15 +0600

    data load

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
+const category = require('./data/category.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+app.get('/category', (req, res) => {
+    res.send(category)
 })
 
 app.listen(port, () => {
